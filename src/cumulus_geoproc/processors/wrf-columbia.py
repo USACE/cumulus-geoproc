9ab4e61a8d3bc0e5acfa595a51e743a0a9ebcdcf
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-09-01 13:55:43 -0500

    WIP: Add pyresample to requirments and processor

--- src/cumulus_geoproc/processors/wrf-columbia.py
+++ src/cumulus_geoproc/processors/wrf-columbia.py
@@ -11,15 +11,20 @@ A next-generation mesoscale numerical weather prediction system designed for bot
 """
 
 
+import math
 import os
 import sys
-from datetime import timezone
+from datetime import datetime, timezone
 from tempfile import TemporaryDirectory
+from typing import List
 
+import numpy
 import pyplugs
+from pyresample.bilinear import NumpyBilinearResampler
+from pyresample import geometry
 from cumulus_geoproc import logger
 from netCDF4 import Dataset, date2index, num2date
-from osgeo import gdal
+from osgeo import gdal, osr
 
 
 @pyplugs.register
@@ -52,12 +57,24 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
     """
     outfile_list = []
 
-    xres = yres = 2000
-    bbox = [-2304000, 2034000, -804000, 3624000]
-    proj4_aea = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
-
     if dst is None:
         dst = os.path.dirname(src)
+
+    xmin = -2304000
+    ymin = 2034000
+    xmax = -804000
+    ymax = 3624000
+    xres = 2000
+    yres = 2000
+
+    proj4_albers = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+    srs = osr.SpatialReference()
+    srs.ImportFromProj4(proj4_albers)
+    wkt_albers = srs.ExportToWkt()
+
+    ncols = math.floor((xmax - xmin) / xres)
+    nrows = math.floor((ymax - ymin) / yres)
+    nodata_value = -9999
 
     src_dir = os.path.dirname(src)
     src_filename = os.path.basename(src)
@@ -70,72 +87,103 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
         # extract the single grid from the source and create a temporary netCDF file
         with Dataset(src, "r") as ncsrc:
             nctime = ncsrc.variables["time"]
+            nclat = ncsrc.variables["lat"]
+            nclat_arr = nclat[1:-1, 1:-1]
+            nclon = ncsrc.variables["lon"]
+            nclon_arr = nclon[1:-1, 1:-1]
+
+            # pyresample uses center of lower left pixel
+            xmin_pxl = xmin + (xres * 0.5)
+            ymin_pxl = ymin + (yres * 0.5)
+
+            # Make target arrays for cell centroids
+            # Using "ncols" (and not ncols -1) in order to include that last pixel in np.arrange
+            # These are 1d arrays
+            xcoords = numpy.arange(xmin_pxl, xmin_pxl + (ncols * xres), xres)
+            ycoords = numpy.arange(ymin_pxl, ymin_pxl + (nrows * yres), yres)
+
+            # Source Geometry
+            source_geometry = geometry.SwathDefinition(lons=nclon_arr, lats=nclat_arr)
+
+            # Target Geometry
+            area_extent = (
+                min(xcoords) - xres * 0.5,
+                min(ycoords) - yres * 0.5,
+                max(xcoords) + xres * 0.5,
+                max(ycoords) + yres * 0.5,
+            )
+            target_geometry = geometry.AreaDefinition(
+                "wrf_columbia",
+                "Albers Equal Area",
+                "aea",
+                wkt_albers,
+                len(xcoords),
+                len(ycoords),
+                area_extent,
+            )
+
+            # 5000 left some nodata cells so going with 6000 m
+            resampler = NumpyBilinearResampler(source_geometry, target_geometry, 6000)
+
             for dt in num2date(
                 nctime[:], nctime.units, only_use_cftime_datetimes=False
             ):
                 dt_valid = dt.replace(tzinfo=timezone.utc)
                 idx = date2index(dt, nctime)
+                ncvar = ncsrc.variables["var"][idx]
+                ncvar_arr = ncvar[1:-1, 1:-1]
 
-                # context handling temp dir for extracting data to .nc
-                with TemporaryDirectory(dir=dst) as tmpdir:
-                    ncdst_path = os.path.join(tmpdir, para + str(idx) + ".nc")
-                    # context handling writing to .nc file
-                    with Dataset(ncdst_path, "w") as ncdst:
-                        # Create dimensions
-                        for name, dimension in ncsrc.dimensions.items():
-                            dim_size = 1 if name == "time" else dimension.size
-                            ncdst.createDimension(name, dim_size)
-                        # Create variables, copy data, and set attributes
-                        for name, variable in ncsrc.variables.items():
-                            ncdst.createVariable(
-                                name, variable.dtype, variable.dimensions
-                            )
-                            if name == "var" or name == "time" or name == "times":
-                                ncdst.variables[name][:] = ncsrc.variables[name][idx]
-                            else:
-                                ncdst.variables[name][:] = ncsrc.variables[name][:]
+                if dt != datetime(1980, 1, 12, 17):
+                    continue
 
-                            ncdst.variables[name].setncatts(
-                                ncsrc.variables[name].__dict__
-                            )
-                    # warp the extracted
-                    gdal.Warp(
-                        tiffile := os.path.join(
-                            dst,
-                            ".".join(
-                                [
-                                    product_slug,
-                                    dt_valid.strftime("%Y%m%d%H"),
-                                    "tif",
-                                ]
-                            ),
+                # resample to target
+                ncvar_arr_resampled = resampler.resample(ncvar_arr, fill_value=nodata_value)
+
+                # Create() GTiff with resampled Albers data
+                raster = gdal.GetDriverByName("GTiff").Create(
+                    "/vsimem/",
+                    xsize=ncols,
+                    ysize=nrows,
+                    bands=1,
+                    eType=gdal.GDT_Float32,
+                )
+                raster.SetGeoTransform([xmin, xres, 0, ymax, 0, -yres])
+                raster.SetProjection(wkt_albers)
+                raster.GetRasterBand(1).SetNoDataValue(nodata_value)
+                raster.GetRasterBand(1).WriteArray(ncvar_arr_resampled)
+
+                raster = None
+
+                # Translate newly created GTiff to COG because COG does not have Create() method
+                gdal.Translate(
+                    tiffile := os.path.join(
+                        dst,
+                        ".".join(
+                            [
+                                product_slug,
+                                dt_valid.strftime("%Y%m%d%H"),
+                                "tif",
+                            ]
                         ),
-                        f"NETCDF:{ncdst_path}:var",
-                        format="COG",
-                        outputBounds=bbox,
-                        outputBoundsSRS=proj4_aea,
-                        xRes=xres,
-                        yRes=yres,
-                        srcSRS="EPSG:4269",
-                        dstSRS=proj4_aea,
-                        outputType=gdal.GDT_Float32,
-                        resampleAlg="bilinear",
-                        creationOptions=[
-                            "COMPRESS=DEFLATE",
-                            "PREDICTOR=2",
-                        ],
-                        dstNodata=-9999,
-                        geoloc=True,
-                    )
+                    ),
+                    f"/vsimem/",
+                    format="COG",
+                    outputType=gdal.GDT_Float32,
+                    resampleAlg="bilinear",
+                    creationOptions=[
+                        "COMPRESS=DEFLATE",
+                        "PREDICTOR=2",
+                    ],
+                )
 
-                    outfile_list.append(
-                        {
-                            "filetype": product_slug,
-                            "file": tiffile,
-                            "datetime": dt_valid.isoformat(),
-                            "version": None,
-                        }
-                    )
+                outfile_list.append(
+                    {
+                        "filetype": product_slug,
+                        "file": tiffile,
+                        "datetime": dt_valid.isoformat(),
+                        "version": None,
+                    }
+                )
     except Exception:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         traceback_details = {
