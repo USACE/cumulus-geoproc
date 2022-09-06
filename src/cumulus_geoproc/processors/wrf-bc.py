d4aaaae64fbd7d225e0695ed4fa9a8b4478e48b8
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-09-06 11:48:44 -0500

    Removed fixed date from processor and tested

--- src/cumulus_geoproc/processors/wrf-bc.py
+++ src/cumulus_geoproc/processors/wrf-bc.py
@@ -131,11 +131,6 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
                 ncvar = ncsrc.variables["var"][idx]
                 ncvar_arr = ncvar[1:-1, 1:-1]
 
-                # ~~~~~~~~~~~~~~~~~ MAKE SURE TO TAKE THIS OUT ~~~~~~~~~~~~~~~~~~~~ #
-                if dt != datetime(1980, 1, 12, 17):
-                    continue
-                # ~~~~~~~~~~~~~~~~~ MAKE SURE TO TAKE THIS OUT ~~~~~~~~~~~~~~~~~~~~ #
-
                 # resample to target
                 ncvar_arr_resampled = resampler.resample(
                     ncvar_arr, fill_value=nodata_value
