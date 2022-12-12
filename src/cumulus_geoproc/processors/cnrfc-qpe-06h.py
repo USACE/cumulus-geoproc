27878980a6b64d92ef229842a488891ec7378e81
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-12-12 13:30:10 -0600

    Slight mod removing VSI reference

--- src/cumulus_geoproc/processors/cnrfc-qpe-06h.py
+++ src/cumulus_geoproc/processors/cnrfc-qpe-06h.py
@@ -47,7 +47,6 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
     }
     ```
     """
-    is_vsi = False
     outfile_list = []
 
     try:
@@ -68,7 +67,6 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
 
         try:
             ds = gdal.Open("/vsigzip/" + src)
-            is_vsi = True
         except RuntimeError as err:
             logger.warning(err)
             logger.warning(f'gunzip "{src}" and use as source file')
@@ -127,9 +125,6 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
             logger.error(f"{k}: {v}")
 
     finally:
-        if is_vsi:
-            gdal.VSIFCloseL(ds)
-        else:
-            ds = None
+        ds = None
     return outfile_list
 
