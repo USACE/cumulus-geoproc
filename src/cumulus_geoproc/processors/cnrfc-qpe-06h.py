49dcbcfc08b5fdd571b4fb2c459ee9d8ed6a3cb9
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2022-12-15 17:20:24 -0600

    Polar Stereographic Geoprocessor for nerc-qpe-06h; Remove test fixture files from image

--- src/cumulus_geoproc/processors/cnrfc-qpe-06h.py
+++ src/cumulus_geoproc/processors/cnrfc-qpe-06h.py
@@ -49,6 +49,15 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
     """
     outfile_list = []
 
+    # projection
+    projection = "+proj=stere +lat_ts=60 +k_0=1 +long_0=-105 +R=6371200 +x_0=0.0 +y_0=0.0 +units=m"
+
+    # Specific to the HRAP Projection
+    # Given a coordinate in HRAP, calculate coordinate in Polar Stereographic
+    # REFERENCE: https://www.weather.gov/owp/oh_hrl_distmodel_hrap
+    ster_x = lambda hrap_x: hrap_x * 4762.5 - 401 * 4762.5
+    ster_y = lambda hrap_y: hrap_y * 4762.5 - 1601 * 4762.5
+
     try:
         # Source and Destination as Paths
         # take the source path as the destination unless defined.
@@ -65,17 +74,66 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
         ):  # 2 or more suffixes assumes pattern like *.tar.gz, *.nc.gz, *.grb.gz, etc
             filename_tif = Path(src_path.stem).with_suffix(".tif")
 
+        # NOTE: Because we are already working against a copy of the acquirable file 'src',
+        #       not the original archive file on AWS S3, use gdal.Open() with gdal.GA_Update
+        #       (not gdal.GA_ReadOnly).
+        #
+        #       This allows setting Transform and Projection on the dataset so they are included
+        #       in the bands that get translated out. Because 'src' is already a copy, no risk of
+        #       corrupting the original file at this time. If 'src' is ever passed as a virtual 
+        #       path to the original file in archive (e.g. /vsis3/...), will want to revisit.
         try:
-            ds = gdal.Open("/vsigzip/" + src)
+            ds = gdal.Open("/vsigzip/" + src, gdal.GA_Update)
         except RuntimeError as err:
             logger.warning(err)
             logger.warning(f'gunzip "{src}" and use as source file')
             src_unzip = utils.decompress(src, str(dst_path))
-            ds = gdal.Open(src_unzip)
+            ds = gdal.Open(src_unzip, gdal.GA_Update)
 
         dataset_meta = gdal.Info(ds, format="json")
-        bands = dataset_meta["bands"]
 
+        # Get Geotransform Information
+        m = dataset_meta["metadata"][""]
+
+        # Initial metadata value for qpe_grid#latLonLL looks like: "{-123.6735229012065,29.94159256439344}"
+        # strip() removes characters "{" and "}"
+        # split() returns a list of two strings (which represent numbers), split on the ","
+        # map() converts each string to a float; list() converts the map object back to a list
+        #
+        # lower left coordinates (minimum x, minimum y)
+        lonLL, latLL = list(map(float, m["qpe_grid#latLonLL"].strip('{}').split(','))) # Lon/Lat
+        hrap_xmin, hrap_ymin = list(map(float, m["qpe_grid#gridPointLL"].strip('{}').split(','))) # HRAP
+        ster_xmin, ster_ymin = ster_x(hrap_xmin), ster_y(hrap_ymin) # Polar Stereographic
+
+        #
+        # upper right coordinates (maximum x, maximum y) in geographic space and pixel space
+        lonUR, latUR = list(map(float, m["qpe_grid#latLonUR"].strip('{}').split(','))) # Lon/Lat
+        hrap_xmax, hrap_ymax = list(map(float, m["qpe_grid#gridPointUR"].strip('{}').split(','))) # HRAP
+        ster_xmax, ster_ymax = ster_x(hrap_xmax), ster_y(hrap_ymax) # Polar Stereographic
+        #
+        # size of the grid
+        # nrows = number of rows in the grid (y or latitude direction)
+        # ncols = number of columns in the grid (x or longitude direction)
+        ncols, nrows = list(map(float, m["qpe_grid#domainExtent"].strip('{}').split(',')))
+
+        # Grid Cell Resolution; polar stereographic reference
+        xres = (ster_xmax - ster_xmin) / float(ncols)
+        yres = (ster_ymax - ster_ymin) / float(nrows)
+
+        # Specify geotransform
+        # https://gdal.org/tutorials/geotransforms_tut.html#introduction-to-geotransforms
+        # GT(0) x-coordinate of the upper-left corner of the upper-left pixel.
+        # GT(1) w-e pixel resolution / pixel width.
+        # GT(2) row rotation (typically zero).
+        # GT(3) y-coordinate of the upper-left corner of the upper-left pixel.
+        # GT(4) column rotation (typically zero).
+        # GT(5) n-s pixel resolution / pixel height (negative value for a north-up image).
+        geotransform = (ster_xmin, xres, 0, ster_ymax, 0, -yres)
+
+        ds.SetGeoTransform(geotransform)
+        ds.SetProjection(projection)
+        
+        bands = dataset_meta["bands"]
         for band in bands:
             band_num = band["band"]
             band_meta = band["metadata"][""]
@@ -91,6 +149,8 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
             cgdal.gdal_translate_w_options(
                 tif := str(dst_path / filename_tif),
                 ds,
+                outputBounds=[lonLL, latUR, lonUR, latLL],
+                outputSRS="EPSG:4326",
                 bandList=[band_num],
                 noData=nodata,
             )
