cab5f5e0e0f99ccb29c86b1038af895f6fe47a45
msweier <mitchell.s.weier@usace.army.mil>
2026-02-20 13:04:00 -0600

    add band finder

--- geoproc/src/cumulus_geoproc/processors/ncep-stage4-mosaic-01h.py
+++ geoproc/src/cumulus_geoproc/processors/ncep-stage4-mosaic-01h.py
@@ -17,7 +17,51 @@ gdal.UseExceptions()
 
 this = os.path.basename(__file__)
 
+def find_band1(data_set: "gdal.Dataset", attr: dict = {}, regex_enabled: bool = False):
+    """Return the band number
 
+    Parameters
+    ----------
+    data_set : gdal.Dataset
+        gdal dataset
+    attr : dict, optional
+        attributes matching those in the metadata, by default {}
+
+    Returns
+    -------
+    int
+        band number
+    """
+    count = data_set.RasterCount
+    for b in range(1, count + 1):
+        has_attr = 0
+        # try:
+        raster = data_set.GetRasterBand(b)
+        meta = raster.GetMetadata_Dict()
+        print(meta)
+        for key, val in attr.items():
+            if key in meta:
+                print(key)
+                # Many grib values include regex special characters. e.g. [C] (degrees celsius)
+                # Function escapes special characters by default to avoid breaking changes by introducing re.search().
+                # Escaping special characters by default will cause this helper function to behave the same way
+                # it did previously when looking for a substring using "in".
+                _val = val
+                if not regex_enabled:
+                    _val = re.escape(_val)
+                if re.search(_val, raster.GetMetadataItem(key)) is not None:
+                    has_attr += 1
+        if has_attr == len(attr):
+            logger.debug(f"{has_attr=}")
+            return b
+
+        # except RuntimeError as ex:
+        #     logger.error(f"{type(ex).__name__}: {this}: {ex}")
+        #     continue
+        # finally:
+            raster = None
+
+    return None
 @pyplugs.register
 def process(*, src: str, dst: str = None, acquirable: str = None):
     """
@@ -61,9 +105,13 @@ def process(*, src: str, dst: str = None, acquirable: str = None):
             dst = os.path.dirname(src)
 
         ds = gdal.Open(src)
+        print(find_band1(ds, attr))
+        band_number = find_band1(ds, attr)
+        band_number = 1
+        print(band_number)
 
-        if (band_number := cgdal.find_band(ds, attr)) is None:
-            raise Exception("Band number not found for attributes: {attr}")
+        # if (band_number := find_band1(ds, attr)) is None:
+        #     raise Exception("Band number not found for attributes: {attr}")
 
         logger.debug(f"Band number '{band_number}' found for attributes {attr}")
 
